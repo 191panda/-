84f63c8008d70283106e83276e4d76126c4526ce
xshun369 <3105805775@qq.com>
2022-05-21 20:48:10 +0800

    封装private

---
+++ textDay5210/src/com/java/day521/School.java
@@ -0,0 +1,13 @@
+package com.java.day521;
+
+public class School {
+     
+	 int timeAge;
+	 
+	 double mainJi;
+	 
+	 String name;
+	 
+	 Studen studen;
+	 
+}
---
+++ textDay5210/src/com/java/day521/Studen.java
@@ -0,0 +1,14 @@
+package com.java.day521;
+
+public class Studen {
+       
+	   //学号 
+	   int studenId;
+	   
+	   int age; 
+	   
+	   String name;
+	   
+	   School school;
+	   
+}
